54824bddf871062115903df43d8cb10d00382937
IonasJospo <ionas.josponis@correo.ucu.edu.uy>
2022-05-15 01:03:21 -0300

    api de mierd

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using Library;
+using TwitterUCU;
 
 namespace PII_Herencia
 {
@@ -11,29 +12,38 @@ namespace PII_Herencia
 
             Person pasajero1 = new Passenger("Pepe", "Rodriguez", 34567890, 10);
             Person pasajero2 = new Passenger("Javi", "nasheer", 34422220, 9);
-            
+
             Person conductor1 = new Driver("Ionas", "Josponis", 52429030, 10, "Volkswagen Gol");
             Person conductorPool1 = new PoolDriver("primo", "del iona", 4566540, 9, "Ford Ranger", 8);
             UcuRideShare rideShare = new UcuRideShare();
-            
+
             rideShare.AddUser(conductor1);
             //Se publica en Twitter un nuevo conductor!
 
             rideShare.AddUser(conductorPool1);
             //Se publica en Twitter un nuevo conductor!
-            
+
             rideShare.AddUser(pasajero1);
             //Se publica en Twitter nuevo registro de pasajero!
-            
+
             rideShare.AddUser(pasajero2);
             //Se publica en Twitter nuevo registro de pasajero!      
 
-                
+
 
             foreach (var user in rideShare.users)
             {
                 Console.WriteLine(user.Name);
-            }  
+            }
+
+            TwitterApi twitterApi = new TwitterApi();
+
+
+            var twitter = new TwitterImage();
+            Console.WriteLine(twitter.PublishToTwitter("text", @"C:\Users\Usuario\OneDrive - Universidad Católica del Uruguay\Escritorio\UCU\SegundoAño\Prog2_2022\EjerciciosIndividuales\PII_HerenciaRideSharing\PII_Herencia_RideSharing_ionasJosponis\bill2.jpg"));
+
+
+
         }
     }
 }
